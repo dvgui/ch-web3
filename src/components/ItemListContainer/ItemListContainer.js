6906ad0a8dbeacbfd43945f8f52d076cd2e3093d
Guido De Vita <guidito.ito@gmail.com>
2021-10-31 20:19:48 -0300

    merged greeting

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -2,14 +2,17 @@ import * as React from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
+import CardContainer from "../CardContainer/CardContainer";
 
-export default function SimpleContainer() {
+export default function SimpleContainer(props) {
   return (
     <React.Fragment>
       <CssBaseline />
       <Container maxWidth="xl">
-        <Box sx={{ bgcolor: '#cfe8fc', height: '90vh' }}>
-        <h1>These are the products we have</h1>
+        <Box sx={{ bgcolor: '#cfe8fc', height: '90vh', padding: '0 1rem' }}>
+        <h1>{props.greeting}</h1>
+        <h2>These are the products we have</h2>
+        <CardContainer />
         </Box>
       </Container>
     </React.Fragment>
